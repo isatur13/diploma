697ebb5be589e80dec8b901937b61c15b38bb495
isatur13 <37103382+isatur13@users.noreply.github.com>
2018-11-20 11:49:50 +0100

    Add files via upload

---
+++ Assets/touchControlLite.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchControlLite : MonoBehaviour {
+
+    Touch touch;
+    CharacterController player;
+    public float maxSpeed;
+    public float jumpForce;
+    float verticalVelocity;
+    Vector3 moveDir;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        anim.SetBool("isWalking", true);
+
+        if (player.isGrounded)
+        {
+            verticalVelocity += Physics.gravity.y;
+            if (Input.touchCount > 0)
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity += Physics.gravity.y;
+        }
+
+        moveDir = new Vector3(0f, verticalVelocity * Time.deltaTime, maxSpeed * Time.deltaTime);
+        player.Move(moveDir);
+	}
+}
